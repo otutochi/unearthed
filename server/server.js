db9872e876d40d01c2e2a522609379be03a1b1c5
Otutochi Nwadinkpa <o.caleb.nwa@gmail.com>
2025-10-01 19:47:05 -0500

    Imported react starter code

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import './config/dotenv.js'
+import cors from 'cors'
 import giftsRouter from './routes/gifts.js'
 
 const app = express();
 
-app.use('/public', express.static('./public'))
-app.use('/scripts', express.static('./public/scripts'))
+app.use(cors())
+
 app.use('/gifts', giftsRouter)
 
 app.get('/', (req, res) => {
